aeb7f5d50fb906af5cb592df33b4bb65d9e24d3e
FU PENG <fupeng@gmail.com>
2012-04-28 10:09:15 +0800

    Initial Commit

---
+++ testpath/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testpath
+//
+//  Created by Peng Fu on 12-4-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ testpath/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testpath
+//
+//  Created by Peng Fu on 12-4-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
